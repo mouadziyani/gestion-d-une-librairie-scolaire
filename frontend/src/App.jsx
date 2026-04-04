bc78e1327abb38eae0adae1fc4d3fbf7bbca91e1
MOEUAED <ziyanimouad@gmail.com>
2026-04-04 23:17:30 +0100

    frontend: update frontend/src/App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Nav from "./components/Navbar";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
 
 function App() {
   return (
     <div>
       <Nav />
+      <hr />
       <h1>Welcome to Librairie BOUGDIM</h1>
+      <hr />
+      <Login />
+      <hr />
+      <Register />
+      <hr />
       <Footer />
     </div>
 
