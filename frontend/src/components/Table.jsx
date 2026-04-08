5308d6abcd41232991deb4c0e8af2422140beaa6
MOEUAED <ziyanimouad@gmail.com>
2026-04-08 22:14:57 +0100

    frontend: update frontend/src/components/Table.jsx

--- frontend/src/components/Table.jsx
+++ frontend/src/components/Table.jsx
@@ -1,6 +1,18 @@
-import React, {useState} from "react";
+import React from "react";
 
-function Table(){
-
+function Table() {
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr><th>Column 1</th><th>Column 2</th><th>Column 3</th></tr>
+        </thead>
+        <tbody>
+          <tr><td>Data A</td><td>Data B</td><td>Data C</td></tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
+
 export default Table;
