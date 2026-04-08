278bff391dda456efe69bc6baa40f26202eee23d
MOEUAED <ziyanimouad@gmail.com>
2026-04-08 22:14:56 +0100

    frontend: update frontend/src/components/SearchBar.jsx

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,6 +1,15 @@
-import React, {useState} from "react";
+import React from "react";
 
-function SearchBar(){
-
+function SearchBar() {
+  return (
+    <div>
+      <form action="#">
+        <label htmlFor="searchInput">Search:</label><br />
+        <input type="text" id="searchInput" name="searchInput" />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
 }
+
 export default SearchBar;
