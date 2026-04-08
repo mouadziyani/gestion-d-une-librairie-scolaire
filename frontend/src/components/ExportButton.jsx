dff7828404a925863099616bd661ee6e3bdd9d1c
MOEUAED <ziyanimouad@gmail.com>
2026-04-08 22:14:55 +0100

    frontend: update frontend/src/components/ExportButton.jsx

--- frontend/src/components/ExportButton.jsx
+++ frontend/src/components/ExportButton.jsx
@@ -1,6 +1,11 @@
-import React, {useState} from "react";
+import React from "react";
 
-function ExportButton(){
-
+function ExportButton() {
+  return (
+    <div>
+      <button type="button">Export Button</button>
+    </div>
+  );
 }
+
 export default ExportButton;
