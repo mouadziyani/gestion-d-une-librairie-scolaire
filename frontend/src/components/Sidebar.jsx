f41e1dd598f5f9c1153d070d29fb10738e63f20a
MOEUAED <ziyanimouad@gmail.com>
2026-04-08 22:14:56 +0100

    frontend: update frontend/src/components/Sidebar.jsx

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,6 +1,18 @@
-import React, {useState} from "react";
+import React from "react";
 
-function SideBar(){
-
+function Sidebar() {
+  return (
+    <div>
+      <aside>
+        <h3>Sidebar</h3>
+        <ul>
+          <li><a href="#">Dashboard</a></li>
+          <li><a href="#">Users</a></li>
+          <li><a href="#">Reports</a></li>
+        </ul>
+      </aside>
+    </div>
+  );
 }
-export default SideBar;
+
+export default Sidebar;
