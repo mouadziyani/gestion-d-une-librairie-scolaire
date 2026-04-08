025fb14019a699b7cbbd65bc30132019c0b017ac
MOEUAED <ziyanimouad@gmail.com>
2026-04-08 22:14:55 +0100

    frontend: update frontend/src/components/Filter.jsx

--- frontend/src/components/Filter.jsx
+++ frontend/src/components/Filter.jsx
@@ -1,6 +1,22 @@
-import React, {useState} from "react";
+import React from "react";
 
-function Filter(){
-
+function Filter() {
+  return (
+    <div>
+      <form action="#">
+        <fieldset>
+          <legend>Filter</legend>
+          <div>
+            <label htmlFor="filter">Keyword:</label><br />
+            <input type="text" id="filter" name="filter" />
+          </div>
+          <div>
+            <button type="submit">Apply</button>
+          </div>
+        </fieldset>
+      </form>
+    </div>
+  );
 }
+
 export default Filter;
