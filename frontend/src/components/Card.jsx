eed771995282a57d4c404343061d05cf907760fb
MOEUAED <ziyanimouad@gmail.com>
2026-04-08 22:14:55 +0100

    frontend: update frontend/src/components/Card.jsx

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,6 +1,15 @@
-import React, {useState} from "react";
+import React from "react";
 
-function Card(){
-
+function Card() {
+  return (
+    <div>
+      <section>
+        <h3>Card</h3>
+        <p>Short description for this card.</p>
+        <button type="button">View Details</button>
+      </section>
+    </div>
+  );
 }
+
 export default Card;
